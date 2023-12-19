5033fa1b627d9d018fe55e8e99a133a720b383f8
oggy107 <urmalsingh10@gmail.com>
2023-12-19 12:21:11 +0530

    added: extended tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,41 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                circularLight:
+                    "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 100px)",
+                circularDark:
+                    "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 100px)",
+
+                circularLightLg:
+                    "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 80px)",
+
+                circularDarkLg:
+                    "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 80px)",
+
+                circularLightMd:
+                    "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 60px)",
+
+                circularDarkMd:
+                    "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 60px)",
+
+                circularLightSm:
+                    "repeating-radial-gradient(rgba(0,0,0,0.4) 2px,#f5f5f5 5px,#f5f5f5 40px)",
+
+                circularDarkSm:
+                    "repeating-radial-gradient(rgba(255,255,255,0.5) 2px,#1b1b1b 8px,#1b1b1b 40px)",
             },
             fontFamily: {
                 montserrat: ["var(--font-montserrat)"],
                 roboto: ["var(--font-roboto)"],
+            },
+            colors: {
+                dark: "#1b1b1b",
+                light: "#f5f5f5",
+                primary: "#B63E96", // 240,86,199
+                primaryDark: "#58E6D9", // 80,230,217
+            },
+            boxShadow: {
+                "3xl": "0 15px 15px 1px rgba(80,230,217, 0.4)",
             },
         },
     },
