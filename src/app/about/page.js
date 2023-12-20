3930283a8181cdae16558b45ae52f8749966b7bb
oggy107 <urmalsingh10@gmail.com>
2023-12-20 16:34:47 +0530

    adjusted: responsive values

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,60 @@
+import Image from "next/image";
+
+import developerPic from "../../../public/images/profile/developer-pic-2.jpg";
+
 export default function About() {
     return (
-        <div>
-            <h1>about page</h1>
+        <div className="w-full min-h-[calc(100vh-8rem)] container m-auto py-7 lg:py-12">
+            <section className="text-center text-[2rem] sm:text-[3rem] whitespace-nowrap lg:text-[5rem] lg:leading-[5rem] font-bold">
+                Passion Fuels Purpose!
+            </section>
+            <section className="my-8 lg:my-16 flex flex-col items-center justify-center md:flex-row gap-4">
+                <div className="w-full">
+                    <h4 className="mb-4 text-xl font-bold text-primary">
+                        BIOGRAPHY
+                    </h4>
+                    <div>
+                        <p>
+                            Hello, I&apos;m{" "}
+                            <span className="font-semibold">
+                                Urmalveer Singh
+                            </span>
+                            , a seasoned full-stack developer proficient in both
+                            web2 and web3 technologies. With a rich background
+                            spanning 4 years, I am dedicated to crafting
+                            aesthetically pleasing, highly functional, and
+                            user-centric digital experiences. Continuously
+                            embracing innovation, I am enthusiastic about
+                            exploring the realms of artificial intelligence and
+                            integrating its advancements into my skill set.
+                        </p>
+                        <p className="py-3">
+                            My goal is to consistently bring my clients&apos;
+                            visions to life through a combination of technical
+                            expertise and a passion for staying at the forefront
+                            of emerging technologies.
+                        </p>
+                        <p>
+                            Whether I&apos;m working on a website, mobile app,
+                            or other digital product, I bring my commitment to
+                            design excellence and user-centered thinking to
+                            every project I work on. I look forward to the
+                            opportunity to bring my skills and passion to your
+                            next project.
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full max-w-[400px] border border-dark rounded-3xl dark:border-light p-5 shadow-[10px_10px] shadow-dark dark:shadow-light transition-shadow duration-300 theme-transition">
+                    <Image
+                        src={developerPic}
+                        alt="developer"
+                        className="w-full rounded-3xl"
+                        quality={100}
+                        placeholder="blur"
+                        priority={true}
+                    />
+                </div>
+            </section>
         </div>
-    )
+    );
 }
